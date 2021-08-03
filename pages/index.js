b6c15afd616345e3d51310bb3ec4f615409748dc
Qu4k3 <paul.guillamon@gmail.com>
2021-08-03 22:46:10 +0200

    change clicable elements to icon buttons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { Eye, MoreVertical } from 'react-feather'
 import users from '../data/array_users.json';
 import AuthLayout from '../layouts/AuthLayout';
 
@@ -14,23 +15,33 @@ export default function Users() {
       <ul>
         {users.items.map(user => (
           <li className="rounded-lg bg-white bg-opacity-5 mb-2" key={user.id}>
-            <Link href={`/${user.id}`}>
-              <a className="flex py-4 px-4">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0 h-10 w-10">
-                    <div className="h-10 w-10 rounded-full bg-white bg-opacity-10"></div>
-                  </div>
-                  <div className="ml-4">
-                    <div className="text-sm font-medium text-gray-500">
-                      {user.name} {user.surname}
-                    </div>
-                    <div className="text-sm text-gray-500">
-                      {user.email}
-                    </div>
-                  </div>
+
+            <div className="flex items-center py-4 px-4">
+              <div className="flex-shrink-0 h-10 w-10">
+                <div className="h-10 w-10 rounded-full bg-white bg-opacity-10"></div>
+              </div>
+              <div className="mx-4">
+                <div className="text-sm font-medium text-gray-200">
+                  {user.name} {user.surname}
                 </div>
-              </a>
-            </Link>
+                <div className="text-sm text-gray-200">
+                  {user.email}
+                </div>
+              </div>
+              <div className="ml-auto flex gap-2">
+                <Link href={`/${user.id}`}>
+                  <a className="flex py-2 px-2 bg-white bg-opacity-5 rounded-2xl">
+                    <Eye className="text-gray-200" />
+                  </a>
+                </Link>
+                <Link href={`/${user.id}`}>
+                  <a className="flex py-2 px-2 bg-white bg-opacity-5 rounded-2xl">
+                    <MoreVertical className="text-gray-200" />
+                  </a>
+                </Link>
+              </div>
+            </div>
+
           </li>
         ))}
       </ul>
