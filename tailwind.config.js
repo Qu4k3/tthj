ff459ce4c647279d0738f5bef4717a821762160f
Qu4k3 <paul.guillamon@gmail.com>
2021-08-03 18:19:27 +0200

    add custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'main': '#081524',
+    }),
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
