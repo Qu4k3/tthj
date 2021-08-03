aab9ac3ebc8628d1564bccfab5d609ce4246b344
Qu4k3 <paul.guillamon@gmail.com>
2021-08-03 18:19:57 +0200

    add auth layout

---
+++ layouts/AuthLayout.js
@@ -0,0 +1,19 @@
+
+export default function AuthLayout({ children }) {
+
+  return (
+    <>
+      <section className="flex justify-between flex-col min-h-screen min-w-full px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-center mt-32">
+          <div className="sm:bg-white sm:bg-opacity-5 rounded-xl p-10">
+            <img className="m-auto pb-8 w-40" src="/logo.svg" alt="" />
+            {children}
+          </div>
+        </div>
+        <footer className="text-gray-500 text-sm text-center py-8">
+          <p>Technical Test Hiberus Javascript</p>
+        </footer>
+      </section>
+    </>
+  )
+}
