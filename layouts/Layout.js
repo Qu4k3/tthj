82b90e3ee275dbfe81112045cfc3bf3613270610
Qu4k3 <paul.guillamon@gmail.com>
2021-08-04 19:20:54 +0200

    change reduce margin top blank space on mobile

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -3,7 +3,7 @@ export default function Layout({ children }) {
   return (
     <>
       <section className="flex justify-between flex-col min-h-screen min-w-full px-4 sm:px-6 lg:px-8">
-        <div className="flex-grow justify-center mt-32 container mx-auto max-w-3xl">
+        <div className="flex-grow justify-center mt-16 sm:mt-32 container mx-auto max-w-3xl">
           <div className="sm:bg-white sm:bg-opacity-5 rounded-xl sm:p-10">
             <img className="m-auto mb-8 w-40" src="/logo.svg" alt="" />
             {children}
