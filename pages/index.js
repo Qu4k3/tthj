3f06c7fd5f65f635fdc122b233b694b4cc4c4423
Qu4k3 <paul.guillamon@gmail.com>
2021-08-04 11:50:32 +0200

    add display users count in top of list

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import users from '../data/array_users.json';
 import Layout from '../layouts/Layout';
 import UserListItem from '../components/elements/UserListItem'
+import { User } from 'react-feather';
 
 export default function Users() {
   return (
@@ -10,7 +11,10 @@ export default function Users() {
         <title key="title">Lista de usuarios | Technical Test Hiberus</title>
       </Head>
 
-      <h1 className="text-xl text-white mb-4">Listado de usuarios</h1>
+      <h1 className=" flex justify-between items-center text-xl text-white mb-4">
+        <span>Listado de usuarios</span>
+        <span className="bg-white bg-opacity-5 px-2 py-1 rounded-lg flex items-center text-lg"><User size="18" className="mr-2" /> {users.count}</span>
+      </h1>
       <ul>
         {users.items.map(user => (
           <li className="rounded-lg bg-white bg-opacity-5 mb-2" key={user.id}>
