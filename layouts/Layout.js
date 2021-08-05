2c98f94b86f9e26888db3254dd341e9a278fbbc5
Qu4k3 <paul.guillamon@gmail.com>
2021-08-05 02:13:32 +0200

    add logout button

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,9 +1,28 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react"
+import { LogOut } from "react-feather"
+import BtnAction from "../components/elements/BtnAction"
+
 export default function Layout({ children }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (sessionStorage.getItem("accessToken") === null) {
+      router.push('/login')
+    }
+  })
+
+  const logout = () => {
+    sessionStorage.removeItem('accessToken')
+    router.push('/login')
+  }
 
   return (
     <>
       <section className="flex justify-between flex-col min-h-screen min-w-full px-4 sm:px-6 lg:px-8">
-        <div className="flex-grow justify-center mt-16 sm:mt-32 container mx-auto max-w-3xl">
+        <div className="flex-grow justify-center mt-16 sm:mt-20 container mx-auto max-w-3xl">
+          <BtnAction onClick={logout} title="Cerrar sesión" styles="text-sm border-0 mb-4 ml-auto" icon={<LogOut size={16} className="mr-2" />} />
           <div className="sm:bg-white sm:bg-opacity-5 rounded-xl sm:p-10">
             <img className="m-auto mb-8 w-40" src="/logo.svg" alt="" />
             {children}
