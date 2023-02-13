659bda8ae4e820cb04914291261fe93fa5a6266b
Paul Guillamon <pguillamon@hiberus.com>
2023-02-13 11:58:10 +0100

    FIX show/hide logout button on user logged state

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,30 +1,43 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react"
-import { LogOut } from "react-feather"
-import BtnAction from "../components/elements/BtnAction"
+import { useEffect, useState } from "react";
+import { LogOut } from "react-feather";
+import BtnAction from "../components/elements/BtnAction";
 
 export default function Layout({ children }) {
-
   const router = useRouter();
+  const [isLogged, setIsLogged] = useState(false)
 
   useEffect(() => {
     if (sessionStorage.getItem("accessToken") === null) {
-      if (!router.pathname.includes("/login") && !router.pathname.includes("/sign-up")) {
-        router.push('/login')
+      setIsLogged(false)
+      if (
+        !router.pathname.includes("/login") &&
+        !router.pathname.includes("/sign-up")
+      ) {
+        router.push("/login");
       }
+    } else {
+      setIsLogged(true)
     }
-  })
+  });
 
   const logout = () => {
-    sessionStorage.removeItem('accessToken')
-    router.push('/login')
-  }
+    sessionStorage.removeItem("accessToken");
+    router.push("/login");
+  };
 
   return (
     <>
       <section className="flex justify-between flex-col min-h-screen min-w-full px-4 sm:px-6 lg:px-8">
         <div className="flex-grow justify-center mt-16 sm:mt-20 container mx-auto max-w-3xl">
-        sessionStorage.getItem("accessToken") !== null && <BtnAction onClick={logout} title="Cerrar sesión" styles="text-sm border-0 mb-4 ml-auto" icon={<LogOut size={16} className="mr-2" />} />
+          {isLogged && (
+            <BtnAction
+              onClick={logout}
+              title="Cerrar sesión"
+              styles="text-sm border-0 mb-4 ml-auto"
+              icon={<LogOut size={16} className="mr-2" />}
+            />
+          )}
           <div className="sm:bg-white sm:bg-opacity-5 rounded-xl sm:p-10">
             <img className="m-auto mb-8 w-40" src="/logo.svg" alt="" />
             {children}
@@ -35,5 +48,5 @@ export default function Layout({ children }) {
         </footer>
       </section>
     </>
-  )
+  );
 }
